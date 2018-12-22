c28743c711b299eb19e08a44f585c1313040492d
juarisco <juarisco@outlook.com>
2018-12-21 20:37:02 -0500

    Profiles table and model

--- resources/views/profiles/profile.blade.php
+++ resources/views/profiles/profile.blade.php
@@ -4,10 +4,17 @@
 <div class="container">
         <div class="col-md-4">
             <div class="panel panel-default">
-                <div class="panel-heading">{{ $user->name }}'s profile</div>
+                <div class="panel-heading">
+                    <p class="text-center">
+                        {{ $user->name }}'s profile.
+                    </p>
+                </div>
 
                 <div class="panel-body">
-                    <img src="{{ Storage::url($user->avatar) }}" width="70px" height="70px" style="border-radius: 50%;" alt="">
+                    <center>
+
+                        <img src="{{ Storage::url($user->avatar) }}" width="70px" height="70px" style="border-radius: 50%;" alt="">
+                    </center>
                 </div>
             </div>
         </div>
